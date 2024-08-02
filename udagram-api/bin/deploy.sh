386b07e1cbb34d1370dd670ec8e1669c9b40d94d
ibrahim Gabr <iageoscience@live.com>
2024-08-03 00:50:24 +0300

    update circle ci

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,12 +1,12 @@
 eb init -p node.js --region us-east-1 udagram-api-app
 
-if aws elasticbeanstalk describe-environments --environment-names udagram-api-proj | grep -q 'udagram-api-proj'; then
-  echo "Environment udagram-api-proj already exists. Skipping creation."
+if aws elasticbeanstalk describe-environments --environment-names udagram-api-backend | grep -q 'udagram-api-backend'; then
+  echo "Environment udagram-api-backend already exists. Skipping creation."
 else
-  eb create --sample udagram-api-proj
+  eb create --sample udagram-api-backend
 fi
 
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET JWT_SECRET=$JWT_SECRET
 
-eb use udagram-api-proj
-eb deploy udagram-api-proj
+eb use udagram-api-backend
+eb deploy udagram-api-backend
