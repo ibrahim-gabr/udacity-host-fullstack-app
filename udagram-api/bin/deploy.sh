5443f28ee37ae7d9b1f0aaca126ea18eddfde376
ibrahim Gabr <iageoscience@live.com>
2024-08-02 20:47:43 +0300

    update circle ci

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,3 @@
-eb init udagram-api --platform node.js --region us-east-1
-eb use udagram-api-env
-eb deploy udagram-api-env
+eb create --region us-east-1 --cname udagram-api
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET JWT_SECRET=$JWT_SECRET
+eb deploy
