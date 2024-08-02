2ab99c30b5e601728667f0a3768961fc278af5ef
ibrahim Gabr <iageoscience@live.com>
2024-08-02 23:09:59 +0300

    update circle ci

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,10 @@
-eb init -p node.js --region us-east-1 udagram-api
-eb create --sample udagram-api-env
+eb init -p node.js --region us-east-2 udagram-api
+
+if aws elasticbeanstalk describe-environments --environment-names udagram-api-env | grep -q 'udagram-api-env'; then
+  echo "Environment udagram-api-env already exists. Skipping creation."
+else
+  eb create --sample udagram-api-env
+fi
+
 eb use udagram-api-env
 eb deploy udagram-api-env
