14e294240b224ba5a8d054bd9ba6be76a46da92e
ibrahim Gabr <iageoscience@live.com>
2024-08-03 14:19:47 +0300

    update circle ci config

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
-eb init udagram2-app --region us-east-1 --platform node.js --instance_profile EMR_EC2_DefaultRole --keyname aws-new --instance-types t2.medium --service-role aws-elasticbeanstalk-ec2-role
-eb create udagram2-app-env
+eb init udagram2-app --region us-east-1 --platform node.js
+eb create udagram2-app-env --instance_profile EMR_EC2_DefaultRole --keyname aws-new --instance-types t2.medium --service-role aws-elasticbeanstalk-ec2-role
 eb use udagram2-app-env
 eb deploy udagram2-app-env
 
