58744a935d9f89e4baa16ab05a49ed145d86c5de
ibrahim Gabr <iageoscience@live.com>
2024-08-03 14:13:57 +0300

    update circle ci config

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,6 +1,6 @@
-eb init udagram1-app --region us-east-1 --platform node.js
-eb create udagram1-app-env
-eb use udagram1-app-env
-eb deploy udagram1-app-env
+eb init udagram2-app --region us-east-1 --platform node.js --instance_profile EMR_EC2_DefaultRole --keyname aws-new --instance-types t2.medium --service-role aws-elasticbeanstalk-ec2-role
+eb create udagram2-app-env
+eb use udagram2-app-env
+eb deploy udagram2-app-env
 
-eb setenv PORT=3000
+eb setenv PORT=$PORT JWT_SECRET=$JWT_SECRET POSTGRESS_USERNAME=$POSTGRESS_USERNAME POSTGRESS_PASSWORD=$POSTGRESS_PASSWORD POSTGRESS_DB=$POSTGRESS_DB POSTGRESS_HOST=$POSTGRESS_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET URL=$URL
